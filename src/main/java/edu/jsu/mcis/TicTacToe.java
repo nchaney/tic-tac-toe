82145556b2ac0f98041ef4dd9bd70a0d435d2a26
Nathan <nchaney@stu.jsu.edu>
2015-09-10 22:33:40 -0500

    Draft, MissingTIEALLFULL test

--- src/main/java/edu/jsu/mcis/TicTacToe.java
+++ src/main/java/edu/jsu/mcis/TicTacToe.java
@@ -1,12 +1,100 @@
 package edu.jsu.mcis;
 
 public class TicTacToe {
-
-
-
-
-
+	
+	mark[][] board = new mark[3][3];
+	
 	public static void main(String[] args) {
 		
 	}
+	public enum mark{XMARK, OMARK, EMPTY}
+	
+	
+	public TicTacToe(){
+		
+		for(int row = 0; row < 3; row++){
+			for(int column = 0; column <3; column++){
+				board[row][column] = mark.EMPTY;
+			}
+		}
+	}
+	
+
+	public void setMark(int row, int column, mark token){
+		board[row][column] = token;
+	}
+	
+	public mark getMark(int row, int column){
+		return board[row][column];
+	}
+	
+	public boolean checkMark (int row, int column){
+		if(board[row][column] != mark.EMPTY){
+			return false;
+		}
+		else
+			return true;
+	}
+	
+	public boolean isFull(){
+		for (int row=0; row<3; row++){
+			for(int column = 0; column <3; column++){
+				if (board[row][column] == mark.EMPTY){
+					return false;
+					
+				}
+				
+					
+			}
+		}
+		return true;
+	}
+	public mark checkForWin () {
+		//check to see if won horizontally
+		if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] != mark.EMPTY) {
+			return board[0][0];
+		}
+		else if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != mark.EMPTY) {
+			return board[1][0];
+		}
+		else if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != mark.EMPTY) {
+			return board[2][0];
+		}
+		//check to see if won vertically
+		else if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != mark.EMPTY) {
+			return board[0][0];
+		}
+		else if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != mark.EMPTY) {
+			return board[0][1];
+		}
+		else if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != mark.EMPTY) {
+			return board[0][2];
+		}
+		//check to see if won diagnally
+		else if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != mark.EMPTY) {
+			return board[0][0];
+		}
+		else if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != mark.EMPTY) {
+			return board[0][2];
+		}
+		//return empty, game has not been won yet
+		else {
+			return mark.EMPTY;
+		}
+	}
+	public boolean checkForTie(){
+		if (isFull() == true && checkForWin() == mark.EMPTY){
+			return true;
+		}
+		return false;
+	}
+	/*public mark resetBoard(){
+		for(int row = 0; row<3; row++){
+			for (int column = 0; column <3; column++){
+				board[row][column] = mark.EMPTY;
+				
+			}
+		}
+	
+	}	*/
 }
--- src/test/java/edu/jsu/mcis/TicTacToeTest.java
+++ src/test/java/edu/jsu/mcis/TicTacToeTest.java
@@ -4,38 +4,75 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TicTacToeTest {
+	
+	private TicTacToe match;
+	
 	@Test
 	public void testInitialBoardIsEmpty() {
-		assertTrue(false);
+		TicTacToe match = new TicTacToe();
+		boolean isEmpty = true;
+		for (int row = 0; row < 3; row++){
+			for (int column = 0; column < 3; column++){
+				if (match.board[row][column] != TicTacToe.mark.EMPTY){
+					isEmpty = false;
+				}
+			}
+		}
+		assertTrue(isEmpty);
 	}
 	
 	@Test
 	public void testMarkXInUpperRightCorner() {
-		assertTrue(false);
+		TicTacToe match = new TicTacToe();
+		match.setMark(0, 2, TicTacToe.mark.XMARK);
+		assertEquals(TicTacToe.mark.XMARK, match.getMark(0,2));
 	}
 	
 	@Test
 	public void testMarkOInBottomLeftCorner() {
-		assertTrue(false);
+		TicTacToe match = new TicTacToe();
+		match.setMark(2, 0, TicTacToe.mark.OMARK);
+		assertEquals(TicTacToe.mark.OMARK, match.getMark(2, 0));
 	}
 	
 	@Test
 	public void testUnableToMarkOverExistingMark() {
-		assertTrue(false);
+		TicTacToe match = new TicTacToe();
+		match.setMark(0,1, TicTacToe.mark.XMARK);
+		assertFalse(match.checkMark(0, 1));
 	}
 	
 	@Test
 	public void testGameIsNotOverAfterTheFirstMark() {
-		assertTrue(false);
+		TicTacToe match = new TicTacToe();
+		boolean gameIsOver = true;
+			if (!match.checkForTie() && match.checkForWin() == TicTacToe.mark.EMPTY){
+				gameIsOver = false;
+			}
+		assertFalse(gameIsOver);
 	}
 	
 	@Test
 	public void testGameIsWonByXHorizontallyAcrossTopRow() {
-		assertTrue(false);
+		TicTacToe match = new TicTacToe();
+		match.setMark(0,0, TicTacToe.mark.XMARK);
+		match.setMark(0,1, TicTacToe.mark.XMARK);
+		match.setMark(0,2, TicTacToe.mark.XMARK);
+		assertEquals(TicTacToe.mark.XMARK, match.checkForWin());
 	}
 	
 	@Test
 	public void testGameIsOverByTieIfAllLocationsAreFilled() {
-		assertTrue(false);
+		TicTacToe match = new TicTacToe();
+		match.setMark(0,0, TicTacToe.mark.XMARK);
+		match.setMark(0,1, TicTacToe.mark.XMARK);
+		match.setMark(1,2, TicTacToe.mark.OMARK);
+		match.setMark(2,0, TicTacToe.mark.OMARK);
+		match.setMark(2,2, TicTacToe.mark.OMARK);
+		match.setMark(0,2, TicTacToe.mark.OMARK);
+		match.setMark(1,0, TicTacToe.mark.XMARK);
+		match.setMark(1,1, TicTacToe.mark.XMARK);
+		match.setMark(2,1, TicTacToe.mark.XMARK);
+		assertFalse(match.checkForTie());
 	}	
 }
